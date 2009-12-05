2a04fca8c2b770e81e40da71abe8ce3949174d14
Jon Ferrer <jonferrer@banshee.dreamhost.com>
2009-12-05 13:23:05 -0800

    add sample geocode REST url

--- settings.php
+++ settings.php
@@ -5,4 +5,8 @@ $googleMapsAPIKey = "ABQIAAAAvYOlX04AjguN5yhxddqxJhQpshiSpNGIWNixjG34yEtU6UEb7hQ
 // ferrinho.com
 // $googleMapsAPIKey = "ABQIAAAAvYOlX04AjguN5yhxddqxJhQ09Fm2IUkCniqCvsZpPhyZNv5wbRSjvoCkrhQVuLYVkywka3u8XUjSng";
 
+// geocode sample url
+// http://maps.google.com/maps/geo?q=155+East+49th+Street,+New+York+,NY+10017&sensor=false&output=json&key=ABQIAAAAvYOlX04AjguN5yhxddqxJhQpshiSpNGIWNixjG34yEtU6UEb7hQOwXCPyhtyFr_aP7BmisoYj5nf1A
+
 ?>
+
